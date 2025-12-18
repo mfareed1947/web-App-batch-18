e0b342676bc7ec5f5b49221980ec978e464fd76f
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2025-12-18 15:15:09 +0500

    JS Class 01

---
+++ 18-12-2025/index.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+// Data Types
+// string
+// Number  
+// boolean
+// Array
+// object
+// Symbol
+// bigint
+// undefined
+// null
+
+// variables
+
+var names;
+console.log(names)
+
+// var obj = {
+//     name: "hamza",
+//     cnic: 4522323535234,
+//     address: "karachi",
+//     picture: null,
+//     contactNumber: 24242342413
+// }
+
+// console.log(obj)

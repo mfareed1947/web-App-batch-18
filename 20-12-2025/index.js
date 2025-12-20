b4c336dfea75a6dcc762687da5084846b35b3102
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2025-12-20 15:01:51 +0500

    JS Class 02

---
+++ 20-12-2025/index.js
@@ -0,0 +1,68 @@
+console.log("hello world")
+
+
+// Data Types
+// String
+// Number
+// Boolean
+// null
+// undefined
+
+
+// Array
+// Object
+// bigInt
+// Symbol
+//  0 vs null vs undefined
+
+// variables rules
+//  not start with number, specail charctr
+// not include space in b/w name
+// var num = true;
+// var %num
+// var phoneNumber = 23452343423;
+// var phone_number = 23452343423;
+// var phonenumber12 = 23452343423;
+// var phone01number = 23452343423;
+
+// var phone@ = 23452343423;
+
+// var rose = true;
+// var Rose = false;
+// console.log(Rose)
+
+
+// var userName;
+// console.log(userName);
+// userName = "fareed";
+// console.log(userName)
+
+// var number = 234.2421231312;
+// console.log(number + 12)
+
+// alert("hello\nworld")
+// var num1 = -prompt("enter your number")
+// var num1 = Number(prompt("enter your number"))
+// var num2 = -prompt("enter your second number")
+// Number(num1)
+// +num1
+
+
+// console.log(num1 + num2)
+
+// -12 + -12
+// -24
+//  234 + - 234
+
+
+// console.log("hello", "world")
+// console.log("hello" + " world")
+// console.log("hello" + " " + "world")
+
+
+
+//
+
+// var num4 = Number(prompt("enter your name"))
+
+// console.log(num4)

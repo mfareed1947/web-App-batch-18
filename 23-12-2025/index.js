65cc82ac9a3aaba9e43fcef40a86e088dea6e606
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2025-12-23 14:36:01 +0500

    JS Class 03

---
+++ 23-12-2025/index.js
@@ -0,0 +1,57 @@
+console.log("hello world");
+
+// previous topics
+// variables
+// data types
+// prompt
+// alert
+// type convertion
+
+//  - + = -
+//  + - = -
+//  - - = +
+//  + + = +
+
+
+// console.log("23" + "23")
+// console.log(23 + 23)
+// console.log(23 - 21)
+// console.log(23 * 21)
+// console.log(23 / 2)
+
+// console.log(+"12" + +"23")
+// console.log(131 % 2)
+
+// var num1 = 12;
+// var num2 = 23;
+
+// console.log(num1 * num2)
+
+// console.log(true)
+// console.log(false)
+
+// console.log(2 == "2")
+// value == value = true
+
+// console.log(2 === "2")
+// value === value + data type
+//   true +  false = false
+
+// console.log(2 === +"2")
+// value === value + data types
+//           true  + true = true
+
+
+// console.log(2 === Number("2"))
+// value === value + data type
+//   true  + true = true
+
+// 659899 === 979899
+
+
+//  +, -, %, / , ==, ===
+// ||, &&
+
+// console.log(20 == 2 || 3 == 0 || 0 == 0 || 1 == 12)
+
+//      true | false

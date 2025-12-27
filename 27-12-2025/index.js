882a199d99db35b03c38153a2a0cae04926ca8a5
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2025-12-27 14:22:31 +0500

    JS Class 04

---
+++ 27-12-2025/index.js
@@ -0,0 +1,38 @@
+console.log("hello")
+
+//  (AND &&) (OR ||)  
+
+// console.log(0 == 1)
+// console.log(1 === 1)
+
+// var userName = "fareed"
+
+// console.log(userName == "Fareed" || 0 == "0" || 1 === 0)
+
+// var userImage = "https://aws.s3.com"
+// var userImage = ""
+// var defaultImage = "./free-user-icon-3296-thumb.png"
+// console.log(userImage || defaultImage)
+// console.log(userImage == 0)
+
+// "" == 0     = true
+// "" == false = true
+// 0 == false  = true
+// userIamge.length == false = true
+// userIamge.length == 0     = true
+
+
+
+// equal to
+// is exist
+
+//  &&
+
+var userName = "adada";
+var email = null;
+var password = null;
+
+
+// console.log(userImage == "" && 0 == 0 && 1 == 1)
+// console.log(userName && email && 0 == 1)
+// console.log(email && password && userName)  

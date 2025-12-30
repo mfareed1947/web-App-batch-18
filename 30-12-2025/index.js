6aca0b94a03b4d72fd9ee403ffff41d29a7a9e32
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2025-12-30 23:06:51 +0500

    JS increment decrement

---
+++ 30-12-2025/index.js
@@ -0,0 +1,83 @@
+// console.log('====================================');
+// console.log("10" === 10);
+// console.log('====================================');
+
+
+// console.log('====================================');
+// console.log(!false);
+// console.log('====================================');
+
+
+
+// console.log('====================================');
+// console.log(5 !== "5");
+// console.log('====================================');
+
+
+// (!== | ===)
+// (!= | ==)
+
+//! && operators required all true 
+
+// console.log('====================================');
+// console.log(false && true && true && true && true)
+// console.log('====================================');
+
+//! || operator required only one true 
+
+// console.log('====================================');
+// console.log(true || false || true || false
+// );
+// console.log('====================================');
+
+
+
+// Pre-Increment / Post-Increment
+// Pre-Decrement / Post-Decrement
+
+// var a = 10;
+
+//! POST INCREMENT
+
+// a++;
+
+//! PRE INCREMENT
+
+// ++a;
+
+// console.log(++a);
+
+//! POST DECREMENT
+
+// a--;
+
+//! PRE DECREMENT
+
+// --a;
+
+// console.log(a--);
+// console.log(a);
+// var a = 10;
+
+var a = 2;
+// console.log((++a + a++) * --a + a--))
+
+
+//! BODMAS 
+
+console.log(++a / a++ + (--a * a--) - a);
+            
+            // 3 / 3 + (3 * 3) - 2
+            // 3 /3 + 9 - 2
+            // 1 + 9 - 2
+            // 10 - 2
+            // 8
+
+            
+
+
+
+// console.log('====================================');
+// console.log(a)
+// console.log('====================================');
+        

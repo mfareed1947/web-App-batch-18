6f13cf2885c5f0109c9045135f9351d4db829824
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-01-03 15:00:59 +0500

    CSS Class 5

---
+++ 03-01-2026 (2)/index.js
@@ -0,0 +1,44 @@
+console.log("hello")
+
+var dukan = true
+
+// if (!dukan) {
+//     console.log("me leke aa raha hon")
+// }
+
+// var gender = "Female"
+
+// if (gender !== "Male") {
+//     console.log("access for female")
+// }
+
+// var tomato;
+// var yugard;
+
+// if (!tomato) {
+//     console.log("tomato")
+// } else {
+//     console.log("yugard")
+// }
+// var water = false
+
+// (water) = true
+// is tomato exist
+
+// (!water)  = true
+// is tomato not exist
+
+
+// if ("Male" !== "Male") {
+
+// }else{
+
+// }
+
+// var number = 2331;
+// Global varible
+
+// if (true) {
+//     let number2 = 43524;
+// }
+// console.log(number2)

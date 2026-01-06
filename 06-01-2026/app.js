2f51225670c2cdf47111dfedc389cc5a9bd735f1
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-01-06 16:40:11 +0500

    JS for loop

---
+++ 06-01-2026/app.js
@@ -0,0 +1,34 @@
+// console.log('====================================');
+// console.log(6 >= 5);
+// console.log('====================================');
+
+//! REMAINING TOPIC 
+
+// FOR LOOP
+
+for(var i = 0; i < 5; i++){
+    console.log(i)
+}
+
+// let i = 0;
+
+// ++i;
+
+// console.log('====================================');
+// console.log(i);
+// console.log('====================================');
+
+
+// var person = "Nabeen";
+// var person1 = "Ali";
+// var person2 = "Hamza";
+
+// var persons = ["Nabeen", "Ali", "Hamza", "Okasha", "Fareed", "Ashar", "Ahsan", "Zubair", "Mustafa"]
+
+// // console.log(persons[0])
+
+// var lastValue = persons.length - 1
+
+// console.log('====================================');
+// console.log(persons[lastValue]);
+// console.log('====================================');

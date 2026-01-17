76c1fbb87856da15bab7634a2dee1e1d2bbf88dc
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-01-17 14:47:17 +0500

    JS nested loop and else if

---
+++ 17-01-2026/app.js
@@ -0,0 +1,62 @@
+// for(var i = 1; i <= 5; i++){
+
+//     console.log("BEFORE EXECUTE INNER LOOP ")
+
+//     for(var j = 1; j <=10; j++){
+//         console.log(`${i} : ${j}`)
+//     }
+
+//     console.log("AFTER EXECUTE INNER LOOP ")
+// }
+
+// var starCount = +prompt("Enter number of lines for star pattern:");
+// for (let i = 1; i <= starCount; i++) {
+
+//     var stars = '';
+
+//     for (let j = 1; j <= i; j++) {
+//         stars += '*';
+//     }
+
+//     console.log(stars);
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+
+// var fullNames = [];
+
+
+// for (var i = 0; i < firstNames.length; i++) {
+
+//   for (var j = 0; j < lastNames.length; j++) {
+//     fullNames.push(firstNames[i] + lastNames[j]);
+//   }
+
+// }
+
+// console.log(fullNames);
+
+
+// var fullName = 'John Doe';
+
+
+// var modifiedName = fullName.slice(2, 4).toUpperCase();
+// console.log(modifiedName);
+
+
+// console.log(fullName);
+
+// ELSE IF 
+
+if(false){
+    console.log("Condition 1 is true");
+}else if(false){
+    console.log("Condition 2 is true");
+}else if(false){
+    console.log("Condition 3 is true");
+}
+else {
+    console.log("No condition is true");
+}

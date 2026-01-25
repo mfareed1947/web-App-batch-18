5bc8f984241f2507da88c05e9da9c27b3bb116d0
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-01-25 18:18:06 +0500

    Add files via upload
    Converting string, numbers and decimals

---
+++ 24-01-2026/app.js
@@ -0,0 +1,23 @@
+var number1 = "5345";
+var number2 = 10;
+number1 = parseInt(number1);
+console.log(number1 + number2);
+// var number = "aasdas";
+
+// console.log(number);
+// number = number.toString();
+// console.log(number);
+// number = parseInt(number);
+// number = Number(number);
+// number = +prompt("enter your age : i.e = 19");
+// console.log(number);
+
+function add(e, d) {
+  var result = e + d;
+  return result;
+}
+
+console.log(add(35, 10));
+function clickAble(name) {
+  alert("hello " + name);
+}

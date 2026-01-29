58e3836f721a57b4ff7bdc1874335f095265812b
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-01-29 15:17:54 +0500

    Context Execution

---
+++ 29-01-2026/app.js
@@ -0,0 +1,39 @@
+// Function definition / statement
+// var firstName;
+// console.log("Before", firstName);
+
+// firstName = "John";
+
+// console.log("After", firstName);
+    // greet("Ali");
+
+    // function greet(name){
+    //     console.log(`Hello, ${name}!`);
+    // }
+
+
+
+// function call/ invoke
+
+// greet("Alice");
+// greet()
+// greet()
+
+// var sum = 20;
+
+function add(num1, num2){
+    var sum = num1 + num2;
+    return sum;
+}
+
+var result1 = add(5, 10);
+console.log("Result 1:", result1);
+
+
+// console.log("After calling add function", sum);
+// add(5, 10); 
+// add(20, 30);
+
+
+
+

c082a2874202d978e46e29e284c43497b2542356
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-03-30 23:06:01 +0500

    JS Revision Class 1

---
+++ 28-03-2026/app.js
@@ -0,0 +1,49 @@
+// var a = 10
+
+// console.log(a++ + ++a)
+// console.log(a)
+
+// var confirmation = confirm("Are you sure?")
+// console.log(confirmation    )
+
+// if(true){
+
+//     console.log("IF CHECK !")
+// }else if(true){
+//     console.log("ELSE IF CHECK !")
+// }else {
+//     console.log("ELSE CHECK !")
+// }
+
+// var a = 18;
+// var b = 18;
+// console.log(a >= b)
+
+// console.log(false && true && true && true)
+// console.log(true || false || false || false)
+
+var fruits = ["Apple", "Banana", "Orange", "Grapes"];
+
+var modifiedFruits = fruits.slice(1, 4);
+console.log(modifiedFruits);
+
+var i = 0;
+
+// while (true) {
+//   // #####
+
+//   console.log(i);
+//   i++;
+// }
+
+
+// do{
+// console.log("FIRST EXECUTION !")
+// }while(false)
+
+
+var num = "2121212.282a223232"
+
+console.log(parseFloat(num))
+
+// console.log(Math.round(num))

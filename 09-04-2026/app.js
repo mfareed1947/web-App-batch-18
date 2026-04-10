9912d457e7f18fbe3c5a217262cc96d6aa40f825
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-04-10 15:50:26 +0500

    Execution context and Hoistiing

---
+++ 09-04-2026/app.js
@@ -0,0 +1,78 @@
+// function a(num1, num2) {
+//     console.log("Execute A ")
+
+//     function b() {
+//         console.log("Execute B")
+
+//         function c() {
+//             console.log("Execute C")
+
+//             console.log(num1 + num2)
+//         }
+
+//         c()
+
+//         console.log("C execution completed !!!!")
+
+//     }
+
+//     b()
+
+//     console.log("B execution completed !!!!")
+// }
+
+// a(10, 20)
+// console.log("A execution completed ")
+
+
+
+// console.log(a)
+
+// var a = 10
+// a()
+
+// function a() {
+//     console.log("Execute A ")
+// }
+
+
+// var a = 10
+
+// // var a = 20
+
+// var a = 10
+
+// function kch_bhi() {
+//     a = 20
+//     console.log(a)
+// }
+
+
+
+// console.log(a)
+// kch_bhi()
+// console.log(a)
+
+// function a() {
+//     var b = 10
+//     console.log("Inner function", b)
+// }
+
+// a()
+// console.log("Outer function", b)
+
+
+
+// console.log(a)
+
+// {
+//     let a = 0
+//     console.log(a)
+// }
+
+// const a;
+
+// console.log(a)
+
+const a  = 10;
+console.log(a);

678da619b1955414561b7d8508b0db42a4e0987c
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-04-21 16:58:50 +0500

    let, const and var

---
+++ 16-04-2026/app.js
@@ -0,0 +1,51 @@
+// LET/ CONST 
+
+// sum();
+
+// var a = 10;
+
+// function sum(){
+//     var a = 20;
+//     
+// }
+// 
+
+
+// let a = 10;
+
+// function sum(){
+//     let a = 20;
+//     
+// }
+
+// sum();
+// 
+
+
+// let a = 10;
+// {
+//     // let a = 20;
+//     
+
+//     {
+//         a = 30;
+//         
+//     }
+// }
+
+// 
+
+
+// 
+// const a = 10; 
+
+// a = 20; 
+
+
+
+const SUCCESS = 200;
+const SERVER_ERROR = 500;
+const NOT_FOUND = 404;
+
+
+console.log(SERVER_ERROR);

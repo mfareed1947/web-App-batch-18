6c8a370c087f43c7ce21e766fb27d396fdb5902c
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-04-21 16:57:56 +0500

    Temporal Dead Zone and Shadowing

---
+++ 11-04-2026/app.js
@@ -0,0 +1,57 @@
+// Execution context
+// Call stack
+// Hoisting
+// Temporal dead zone
+// var/ let / const
+// Variable Shadowing
+
+
+// var a = 100
+// function test(){
+//     var a = 10
+//     console.log(a)
+
+// }
+// test()
+
+// console.log(a)
+
+
+// var a = 10
+// var b = 20
+
+// function sum(num1, num2){
+//     var result = num1 + num2
+//     return result
+// }
+
+// console.log(sum(a, b))
+
+
+// let a = 10
+// console.log(a)
+
+
+let a = 10;
+// console.log(a)
+
+
+// let b = 20
+// console.log(b)
+
+// let c = a + b
+// console.log(c)
+
+{
+    let a = 100
+    console.log(a)
+}
+
+// console.log(a)
+
+// {
+//     let a = 100
+//     console.log(a)
+// }
+
+// console.log(a)

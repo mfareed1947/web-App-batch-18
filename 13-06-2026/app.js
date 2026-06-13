5fccfb0fdd2b660fd5671054bfc3be64b04aad40
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2026-06-13 14:51:41 +0500

    Modules

---
+++ 13-06-2026/app.js
@@ -0,0 +1,22 @@
+// import calculator from "./calculator.js"
+
+import {add, subtract}from "./calculator.js"
+
+console.log(add(10, 2))
+console.log(subtract(10, 2))
+
+// const user = {
+//     name: "Ali",
+//     age: 25,
+//     cnic: "1234567890"
+// }
+
+// console.log(user?.cnic)
+
+// const data = undefined
+
+// const result = data?.map((item)=>{
+//     return item
+// })
+
+// console.log(result)
---
+++ 13-06-2026/calculator.js
@@ -0,0 +1,18 @@
+function add(a, b){
+    return a + b;
+}
+
+
+function subtract(a, b){    
+    return a - b;
+}
+
+// export default {
+//     add, 
+//     subtract
+// }
+
+export {
+   add ,
+    subtract
+}
